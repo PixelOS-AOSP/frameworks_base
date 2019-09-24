39ee80fa6b242f92309887666b02b8d5fbad3924
Marko Man <darkobas@gmail.com>
2019-09-24 14:46:01 +0200

    base: Double tap to sleep on statusbar [1/2]
    arrow-11.0 edits:
    - Adapt code to refactored statusbar controllers.
    
    wave edits:
    - Adapt to existing dt2s on lockscreen
    
    commit f3321f6a498b3b55a509524183ce83b7503cf936
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed May 23 21:55:04 2018 +0200
    
        base: SystemUI: prevent that double tap to sleep triggers panel expand
    
        arrow-11.0 edits:
            - Adapt code to refactored statusbar controllers.
    
        http://jira.omnirom.org/browse/OMNI-1466
    
        Change-Id: If304577d666ebf8423cca1298dfbe044843657cc
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    
    [ghostrider-reborn | imjyotiraditya] adapted to android 12
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ibfcd4619654910a95e6d49acbbceb15e21e2d2d0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5522,6 +5522,12 @@ public final class Settings {
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
         /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5651,6 +5657,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**
