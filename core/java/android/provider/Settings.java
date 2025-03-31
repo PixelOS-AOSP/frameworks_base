5ced867d71463706e04ad1c67d5307edb73f1b95
Ashwin R C <ashwin2001achu@gmail.com>
2025-03-31 23:05:36 +0545

    fixup! SystemUI: Adapt screenshot sound to ringer modes
    [SahilSonar - POSP]: Forward port to android-12.0.0
    idoybh: Adapt to A13
    
    Change-Id: I381c351131241e45ddb6049706d6c302c2eee946
    
    base: allow disable of screenshot shutter sound [1/2]
    
    [SahilSonar - POSP]
     - Forward port to android-12.0.0
    
    idoybh: Adapt to A13
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    
    SystemUI: Screenshots: Refactor shutter sound logic
    
    * Commit 2f09bacae02fe7a99d62355331c3a26b83752a6a introduced some
      duplicated code
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    [SahilSonar - POSP]
      - Forward port to androi-12.0.0
    
    idoybh: Adapt to A13
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    
    SystemUI: Fix shutter sound
    
    * When shutter sound for camera is forced on as required in some states,
      (config_camera_sound_forced, set via mcc/mnc), we also want to
      (or should) play it when a screenshot is taken from the preview
      instead of an actual picture
    * This change is loosely based on
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1517742/
      but uses publicly available APIs
    
    Testing:
    - Set config_camera_sound_forced to true and push a build to device
    - Turn down all stream volumes to muted
    - Take screenshot of any normal screen
      -> No sound played
    - Open camera, take screenshot
      -> Sound played
    - Turn up volume and repeat the screenshots
      -> Sound played in all cases
    
    [SahilSonar - POSP]
     - Forward port to android-12.0.0
    
    idoybh: Adapt to A13
    
    @ neobuddy89:
    * Remove enforcing shutter sound via prop.
    * Adapt for A15 QPR1 new screenshot controller
    
    Change-Id: I381c351131241e45ddb6049706d6c302c2eee946
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6639,11 +6639,6 @@ public final class Settings {
          */
 
         /**
-         * @hide
-         */
-        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
-
-        /**
          * Whether the torch launch gesture when the screen is off should be enabled.
          * @hide
          */
@@ -6709,6 +6704,11 @@ public final class Settings {
          * @hide
          */
         public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
